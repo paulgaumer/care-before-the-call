bef04ea12916a9aff1be5c364ef417e2308db650
Paul Gaumer <mitsudomoe8@gmail.com>
2020-01-17 17:54:41 +0100

    Add background color to behindCampaign section

--- src/components/Behind-campaign/behindCampaign.js
+++ src/components/Behind-campaign/behindCampaign.js
@@ -8,24 +8,28 @@ import logoCity from '../../images/office_on_health_badge.svg';
 const BehindCampaign = () => {
   const { portraits } = portraitsList;
   return (
-    <section className="container">
-      <div className={styles.topSection}>
-        <img src={logoCity} alt="City of Central Fall" />
-        <div className={styles.textSide}>
-          <h2>
-            <FormattedMessage id="behindCampaign.who-behind-campaign.title" />
-          </h2>
-          <p>
-            <FormattedMessage id="behindCampaign.who-behind-campaign.content" />
-          </p>
+    <div className={styles.wrapper}>
+      <section className="container">
+        <div className={styles.topSection}>
+          <img src={logoCity} alt="City of Central Fall" />
+          <div className={styles.textSide}>
+            <h2>
+              <FormattedMessage id="behindCampaign.who-behind-campaign.title" />
+            </h2>
+            <p>
+              <FormattedMessage id="behindCampaign.who-behind-campaign.content" />
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="row">
-        {Object.keys(portraits).map(portrait => {
-          return <Portrait character={portrait} key={portrait} image={portraits[portrait].image} />;
-        })}
-      </div>
-    </section>
+        <div className="row">
+          {Object.keys(portraits).map(portrait => {
+            return (
+              <Portrait character={portrait} key={portrait} image={portraits[portrait].image} />
+            );
+          })}
+        </div>
+      </section>
+    </div>
   );
 };
 
