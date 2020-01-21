419fb8862298181c998b8e8c497a506216cb7f5f
Paul Gaumer <mitsudomoe8@gmail.com>
2020-01-21 11:08:40 +0100

    Adapt responsivness on hero

--- src/components/Logo-hero/logoHero.js
+++ src/components/Logo-hero/logoHero.js
@@ -4,13 +4,33 @@ import styles from './logoHero.module.scss';
 
 import logoCare from '../../images/cbtc_logo_english.svg';
 import logoCity from '../../images/office_on_health_badge.svg';
+import chevronDown from '../../images/icons/chevron-down.svg';
 
 const LogoHero = () => {
   return (
     <section className={styles.section}>
       <div className="container">
-        <div className="row">
-          <div className="col-sm-12 col-md-7">
+        {/* <div className="row"> */}
+        <div className={styles.hero}>
+          <div className={styles.logoSide}>
+            <div className={styles.logoGroup}>
+              <img src={logoCare} alt="Care Before The Call" className={styles.logoCare} />
+              <div className={styles.cityLogo}>
+                <div className={styles.legend}>
+                  <p>Brought to you by the</p>
+                  <p>Central Falls Office on Health</p>
+                </div>
+                <img src={logoCity} alt="City of Central Falls" />
+              </div>
+            </div>
+            <div className={styles.chevronDown}>
+              <a href="#heroText">
+                <img src={chevronDown} alt="chevron down" />
+              </a>
+            </div>
+          </div>
+
+          <div className={styles.textSide} id="heroText">
             <div className={styles.paragraphe}>
               <h2>
                 <FormattedMessage id="logoHero.what-is-btc.title" />{' '}
@@ -35,19 +55,8 @@ const LogoHero = () => {
               </p>
             </div>
           </div>
-          <div className="col-sm-12 col-md-5">
-            <div className={styles.logoSide}>
-              <img src={logoCare} alt="Care Before The Call" />
-              <div className={styles.cityLogo}>
-                <div className={styles.legend}>
-                  <p>Brought to you by the</p>
-                  <p>Central Falls Office on Health</p>
-                </div>
-                <img src={logoCity} alt="City of Central Falls" />
-              </div>
-            </div>
-          </div>
         </div>
+        {/* </div> */}
       </div>
     </section>
   );
