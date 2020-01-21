8f92ce1aba5fa8f3ff8e52ae5ba4ae254a8185ef
Paul Gaumer <mitsudomoe8@gmail.com>
2020-01-21 17:59:29 +0100

    Add social icons to the mobile menu

--- src/components/Header/mobile/credits.jsx
+++ src/components/Header/mobile/credits.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import styles from './credits.module.scss';
+import Socials from '../../socials';
 import citySeal from '../../../images/city_seal.svg';
+import instagramBlack from '../../../images/icons/instagram-black.svg';
+import twitterBlack from '../../../images/icons/twitter-black.svg';
+import facebookBlack from '../../../images/icons/facebook-black.svg';
 
 const Footer = () => {
   return (
@@ -17,6 +21,18 @@ const Footer = () => {
         <p>
           <FormattedMessage id={`follow.content`} />
         </p>
+        <div className={styles.creditsSocials}>
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src={instagramBlack} alt="instagram" />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src={facebookBlack} alt="facebook" />
+          </a>
+
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src={twitterBlack} alt="twitter" />
+          </a>
+        </div>
         <p>
           <FormattedMessage id={`footer.copyright`} />
         </p>
